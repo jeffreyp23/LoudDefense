82a60cdb1decd93458c7393ace6a23cc299b33c2
Marcel Vincourt <marcel@e-trailer.nl>
2017-05-17 17:15:42 +0200

    we krijgen en hash nu nog testen met nieuwe code maar er is nog een ES

---
+++ cplcd/examples/cpp_embedded/eClient/PLC.cpp
@@ -0,0 +1,96 @@
+#include "PLC.h"
+#include <vector>
+#include <sstream>
+
+PLC::PLC(std::string ipAddress)
+{
+	client = new TSnap7Client;
+	int res = client->ConnectTo(ipAddress.c_str(), 0, 2);
+	if (res != 0)
+	{
+		throw "geen verbinding";
+	}
+}
+
+std::string PLC::getCode()
+{
+	TS7BlocksList *List;
+	List = getBlockList();
+	
+
+	if (List == nullptr){
+		return nullptr;
+	}
+
+	std::stringstream ss;
+	if (List->OBCount > 0)
+	{
+		ss << this->processBlocks(Block_OB, List->OBCount);	
+	}
+
+	if (List->FBCount > 0)
+	{
+		ss << this->processBlocks(Block_FB, List->FBCount);
+	}
+
+	if (List->FCCount > 0)
+	{
+		ss << this->processBlocks(Block_FC, List->FCCount);
+	}
+
+	if (List->SFBCount > 0)
+	{
+		ss << this->processBlocks(Block_SFC, List->SFBCount);
+	}
+	if (List->SFCCount > 0)
+	{
+		ss << this->processBlocks(Block_SFC, List->SFCCount);
+	}
+
+	delete List;
+	return ss.str();
+}
+
+TS7BlocksList* PLC::getBlockList()
+{
+	TS7BlocksList* List = new TS7BlocksList;
+	int res = client->ListBlocks(List);
+
+	if (res == 0) {
+		return List;
+	}
+	else {
+		delete List;
+		return nullptr;
+	}
+}
+
+std::string PLC::processBlocks(byte Block_Type, int count)
+{
+	byte Buffer[65536]; // 64 K buffer
+	int Size = sizeof(Buffer); // Size is IN/OUT par
+							   // In input it tells the client the size available
+							   // In output it tells us how many bytes were uploaded.
+	std::vector<char*> blocks;
+	for (int i = 0; i < count; i++)
+	{
+		int res = client->Upload(Block_Type, i, &Buffer, Size);
+		if (res == 0)
+		{
+			blocks.push_back((char*)(Buffer));
+		}
+	}
+
+	std::stringstream ss;
+	for (std::vector<char*>::iterator it = blocks.begin(); it != blocks.end(); ++it) {
+		ss << *it;
+	}
+
+	return ss.str();
+}
+
+PLC::~PLC()
+{
+	client->Disconnect();
+	delete client;
+}
